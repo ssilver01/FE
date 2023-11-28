62abd5eccbeac6ae8ae669b07712d974a1f5555c
chrin05 <rinring105@gmail.com>
2023-11-28 23:47:48 +0900

    feat : nav bar변경 및 파일 이름 수정

--- app/src/main/java/com/example/youxmap/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/example/youxmap/ui/mypage/MyPageViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.youxmap.ui.dashboard
+package com.example.youxmap.ui.mypage
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class MyPageViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/youxmap/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/youxmap/ui/quiz/QuizFragment.kt
@@ -1,4 +1,4 @@
-package com.example.youxmap.ui.dashboard
+package com.example.youxmap.ui.quiz
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.youxmap.databinding.FragmentDashboardBinding
+import com.example.youxmap.databinding.FragmentQuizBinding
 
-class DashboardFragment : Fragment() {
+class QuizFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentQuizBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val quizViewModel =
+            ViewModelProvider(this).get(QuizViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentQuizBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        quizViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
