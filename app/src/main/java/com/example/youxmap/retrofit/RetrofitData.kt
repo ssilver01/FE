e074ed2b26896f4188edf5692dfe55f7ca4c3515
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2023-12-02 04:00:34 +0900

    feat : Retrofit 추가

--- app/src/main/java/com/example/youxmap/retrofit/RetrofitData.kt
+++ app/src/main/java/com/example/youxmap/retrofit/RetrofitData.kt
@@ -11,3 +11,35 @@ data class User (
     @SerializedName("nickname") val nickname: String,
     @SerializedName("profile_img") val profile_img: Int
 )
+data class UserLogin(
+    @SerializedName("id") val id: String,
+    @SerializedName("pwd") val pwd: String,
+)
+data class UserLogout(
+    @SerializedName("id") val id: String,
+    @SerializedName("pwd") val pwd: String,
+)
+
+data class UserEdit(
+    @SerializedName("pwd") val pwd: String,
+    @SerializedName("nickname") val nickname: String,
+    @SerializedName("profile_img") val profile_img: Int
+)
+
+data class  UserIdSearch(
+    @SerializedName("phone_number") val phone_number: String,
+)
+data class  UserPwdSearch(
+    @SerializedName("id") val id: String,
+    @SerializedName("email") val email: String
+)
+data class Quiz (
+
+
+)
+
+data class Ranking(
+    @SerializedName("total_coin") val total_coin: Int,
+    @SerializedName("nickname") val nickname: String,
+    @SerializedName("profile_img") val profile_img: Int
+)
