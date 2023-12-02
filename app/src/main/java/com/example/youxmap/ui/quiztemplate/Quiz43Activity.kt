f51aa1307b54073bd92ffc4a3c7332c41e6bd752
chrin05 <rinring105@gmail.com>
2023-12-03 04:22:49 +0900

    feat : quiz 코틀린 파일 추가

--- app/src/main/java/com/example/youxmap/ui/quiztemplate/Quiz43Activity.kt
+++ app/src/main/java/com/example/youxmap/ui/quiztemplate/Quiz43Activity.kt
@@ -3,15 +3,16 @@ package com.example.youxmap.ui.quiztemplate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.youxmap.databinding.ActivityQuiz11Binding
-import com.example.youxmap.databinding.ActivityQuizAnswerBinding
+import com.example.youxmap.databinding.ActivityQuiz43Binding
 
-class Quiz11Activity : AppCompatActivity() {
-    private lateinit var binding: ActivityQuiz11Binding
+class Quiz43Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityQuiz43Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityQuiz11Binding.inflate(layoutInflater)
+        binding = ActivityQuiz43Binding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
 
 }
